5196a82dd3e0bc2ef66d9703c9bc4fd4f4809610
kaneraZh <ari1337ferrada@gmail.com>
2024-02-19 21:29:54 -0300

    Update focus_inheritance.gd
    focus inheritance, to control menus by inheritance, in case you wanna block access to certain menus, controlling just by telling a root node

--- scripts/gui/focus_inheritance.gd
+++ scripts/gui/focus_inheritance.gd
@@ -1,1 +1,21 @@
-extends Node
+extends Resource
+class_name FocusInheritanceController
+
+@export var mode:Control.FocusMode = Control.FOCUS_ALL
+var focus_children:Array[Control]
+var root:Node=null : set=set_root
+func set_root(node:Node)->void:
+	root = node
+	focus_children = find_focus_children(root)
+func find_focus_children(parent:Node)->Array[Control]:
+	var res:Array[Control] = []
+	for n:Node in parent.get_children(true):
+		if(n is Control):
+			if(n.get_focus_mode(mode)):res.append(n)
+		if(n.get_child_count(true)>0):
+			res.append_array( find_focus_children(n) )
+	return res
+
+func set_mode_children(focus_mode:Control.FocusMode)->void:
+	for n:Control in focus_children:
+		n.set_focus_mode(focus_mode)
